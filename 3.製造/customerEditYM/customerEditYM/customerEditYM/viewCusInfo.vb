f3f88042f9774c822a36999bfc6e3fba5db37c5d
yamashiro-masayuki <92512442+yamashiro-masayuki@users.noreply.github.com>
2021-12-24 16:20:08 +0900

    21/12/24 プロジェクトを全て合わせたソリューションの追加

---
+++ 3.製造/customerEditYM/customerEditYM/customerEditYM/viewCusInfo.vb
@@ -0,0 +1,22 @@
+﻿Public Class viewCusInfo
+
+
+    'ログイン者名前
+    Public Property viewCusID As String = "admin"
+
+    'ロックエラーフラグ
+    Public Property rockError As Boolean = False
+
+
+    '性別の文字化
+    Public Property sexWord As String
+
+
+
+
+
+
+
+
+
+End Class
---
+++ 3.製造/customerEditYM/customerEditYM/customerEditYM/webMainMenu.aspx.vb
@@ -0,0 +1,31 @@
+﻿Public Class webMainMenu
+    Inherits System.Web.UI.Page
+
+    Dim commonLoginCus As New loginCusInfo
+
+    'メインメニューが表示された時に起こる処理
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        lbl_LoginName.Text = $"{commonLoginCus.loginLastName}{commonLoginCus.loginName} さん"
+
+    End Sub
+
+    'ログアウトボタンの押下処理
+    Protected Sub btn_LogOut_Click(sender As Object, e As EventArgs) Handles btn_LogOut.Click
+
+
+
+
+    End Sub
+
+    '会員情報登録ボタンの押下処理
+    Protected Sub btn_addCus_Click(sender As Object, e As EventArgs) Handles btn_addCus.Click
+
+    End Sub
+
+    '会員情報一覧表示ボタンの押下処理
+    Protected Sub btn_viewCus_Click(sender As Object, e As EventArgs) Handles btn_viewCus.Click
+
+    End Sub
+
+End Class
